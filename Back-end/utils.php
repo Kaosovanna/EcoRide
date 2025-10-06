070fffabe9cd48ca0029646f4e72f77d8282fb9e
Kaosovanna <kaosovanna.pas@gmail.com>
2025-10-06 20:44:10 +0200

    Delete Back-end directory

--- Back-end/utils.php
+++
@@ -1,9 +0,0 @@
-<?php
-// utils.php - fonctions utilitaires simples
-function json_input(){
-  $data = file_get_contents('php://input');
-  return json_decode($data, true) ?: [];
-}
-function respond($arr){ header('Content-Type: application/json'); echo json_encode($arr); exit; }
-function hash_pass($p){ return password_hash($p, PASSWORD_DEFAULT); }
-?>
